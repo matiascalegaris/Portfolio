7b8215ab332abc5a8cf972cc2f9076785ea1187d
Matias <calegaris.matias@gmail.com>
2024-04-11 16:12:08 -0300

    add image content to ds preview

--- src/Components/Portfolio/ProjectDetails/DSProjectPreview/ds-project-preview.tsx
+++ src/Components/Portfolio/ProjectDetails/DSProjectPreview/ds-project-preview.tsx
@@ -2,14 +2,15 @@ import "./ds-project-preview.scss"
 import { DSProject } from "../../../../Models/project"
 
 export const DSProjectPreview = ({projectDetails} : {projectDetails : DSProject}) => {
-  const dsStyle : React.CSSProperties = {
-    maxWidth: `500px`,
-  }
+
   return (
     <article className="ds-grid">
       <h2 className="main-grid">{projectDetails.name}</h2>
       <p className="main-grid">{projectDetails.summary}</p>
-
-      <img style={dsStyle} className="secondary-grid" src="/portfolio-resources/NintendoDSImage.png"/>
+      <div className="ds-area secondary-grid">
+        <div className="top-screen"><img src={projectDetails.images[0]}/></div>
+        <div className="bottom-screen"><img className="bottom-img" src={projectDetails.images[0]}/></div>
+        <img className="ds" src="/portfolio-resources/NintendoDSImage.png"/>
+      </div>
     </article>
   )}
